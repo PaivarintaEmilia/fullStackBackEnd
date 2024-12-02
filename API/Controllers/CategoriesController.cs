6be42ce5eb623c85d13c7021c9a38f6aea8c9490
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-12-02 09:32:11 +0100

    Add comments to cateogry implementation and delete unnecessary debug functions

--- API/Controllers/CategoriesController.cs
+++ API/Controllers/CategoriesController.cs
@@ -18,39 +18,35 @@ namespace API.Controllers
         [HttpPost("addCategory")]
         public async Task<ActionResult<CategoryResDto>> CreateCategory(CategoryReqDto req)
         {
-            // Debug
-            foreach (var claim in User.Claims)
-            {
-                Console.WriteLine($"Claim Type: {claim.Type}, Value: {claim.Value}");
-            }
 
-            // Hae käyttäjän ID
+            // Hae käyttäjän ID tokenista
             var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ?? User.FindFirst("sub");
-            Console.WriteLine($"userIdClaim: {userIdClaim}");
 
             if (userIdClaim == null)
             {
                 return Unauthorized("User ID ei löytynyt tokenista.");
             }
 
+            // Muutetaan id kokonaisluvuksi
             var userId = int.Parse(userIdClaim.Value);
+            
             var newCategory = await service.Create(req, userId);
 
             return Ok(newCategory);
         }
 
-        // Kategorian muokkaaminen
+        // Kategorian muokkaaminen | vain adminit 
         [Authorize(Roles = "admin")]
         [HttpPatch("{id}")] // Vain osittaiseen muokkaukseen
         public async Task<ActionResult<CategoryResDto?>> UpdateCategory(CategoryReqDto req, int id)
         {
             // En ottanut id:tä mukaan, koska en tiedä onko sillä väliä. Riippuu siitä halutaanko pitää tieto categorian luojasta tietokannasta vai 
-            // viimeisimmästä muokkaajasta
+            // viimeisimmästä muokkaajasta. Tästä löytyy lisätietoa ReadMe-filusta. 
             var updatedCategory = await service.Update(req, id);
             return Ok(updatedCategory);
         }
 
-        // Kategorian poisto
+        // Kategorian poisto | vain adminit 
         [Authorize("admin")]
         [HttpDelete("{id}")]
         public async Task<ActionResult<string?>> DeleteCategory(int id)
